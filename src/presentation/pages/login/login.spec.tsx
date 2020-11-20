28d72b15e01c73142a1bf42bb1c060a4b24bf532
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-11-20 14:04:40 -0500

    test: ensure login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -58,6 +58,15 @@ describe('Login Component', () => {
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
+  test('Should show valid email state if Validations succeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo certo!')
+    expect(emailStatus.textContent).toBe('🟢')
+  })
   test('Should show valid password state if Validations succeds', () => {
     const { sut, validationStub } = makeSut()
     validationStub.errorMessage = null
