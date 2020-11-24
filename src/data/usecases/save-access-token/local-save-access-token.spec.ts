fc73e0d355a51c675405a2af155c450356af054e
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-11-24 16:18:31 -0300

    refactor: rename dataa mocks

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,7 +1,7 @@
 
 import faker from 'faker'
 import { LocalSaveAccessToken } from './local-save-access-token'
-import { SetStorageMock } from '@/data/test/mock-storage'
+import { SetStorageMock } from '@/data/test'
 
 type SutTypes = {
   sut: LocalSaveAccessToken
