2a8df2d93714fac50018af3755d1faff9d1664fe
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-11-24 08:28:53 -0300

    chore: add files to coverage ignore

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
 import 'jest-localstorage-mock'
-import Login from './login'
+import { Login } from '@/presentation/pages'
 import {
   render,
   RenderResult,
