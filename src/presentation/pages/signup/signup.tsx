bce1c9484bd17c7fb9b3d51e4704203228093054
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-11-25 16:10:30 -0300

    feat: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -37,6 +37,7 @@ const SingUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if (state.isLoading) return
     setState({
       ...state,
       isLoading: true
