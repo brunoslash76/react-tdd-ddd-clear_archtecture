73cd0b3ec29fbf0a2a466e1093be3022e9341931
Bruno Russo <bruno.russo-ext@viavarejo.com.br>
2020-11-25 16:10:51 -0300

    test: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -135,4 +135,11 @@ describe('Login Component', () => {
       passwordConfirmation
     })
   })
+
+  test('Should call AddAccount only once', async () => {
+    const { sut, addAccountSpy } = makeSut()
+    await simulateValidSubmit(sut)
+    await simulateValidSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(1)
+  })
 })
