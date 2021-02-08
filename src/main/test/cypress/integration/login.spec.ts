c9ad1e2a588da98c9536142db137d2ef41ff2e4c
Bruno Russo <bruno.russo-c@adventhealth.com>
2021-02-08 17:17:30 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,4 +1,5 @@
-import { truncate } from 'cypress/types/lodash'
+
+import faker from 'faker'
 
 describe('Login', () => {
   beforeEach(() => {
@@ -11,6 +12,21 @@ describe('Login', () => {
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('submit').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴')
+
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('submit').should('not.have.descendants')
   })
--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -28,7 +28,7 @@ const Input: React.FC<Props> = (props: Props) => {
 
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} data-testid={props.name} onChange={handleChange}/>
+      <input {...props} data-testid={props.name} onChange={handleChange} />
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
