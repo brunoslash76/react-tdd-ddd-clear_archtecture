f5fee183c98b59acc7cafe65115dc667bf23da68
Bruno Russo <bruno.russo-c@adventhealth.com>
2021-02-25 16:08:33 -0300

    feat: ensure AuthorizeHttpGetClientDecorator returns the same as HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -16,7 +16,6 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
         })
       })
     }
-    await this.httpGetClient.get(params)
-    return null
+    return this.httpGetClient.get(params)
   }
 }
