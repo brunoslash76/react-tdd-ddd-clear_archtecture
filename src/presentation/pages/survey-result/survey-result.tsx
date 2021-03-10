7488c5901c65b4ba607e6f95f402893bde8a0b2d
Bruno Russo <bruno.russo-c@adventhealth.com>
2021-03-10 15:06:00 -0300

    feat: add Calendar to SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import FlipMove from 'react-flip-move'
-import { Footer, Header } from '@/presentation/components'
+import { Calendar, Footer, Header } from '@/presentation/components'
 import Loading from '@/presentation/components/loading/loading'
 import Styles from './survey-result-styles.scss'
 
@@ -9,7 +9,10 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <h2>Qual é seu framework web favorito?</h2>
+        <hgroup>
+          <Calendar className={Styles.calendarWrap} date={new Date()}/>
+          <h2>Qual é seu framework web favorito?</h2>
+        </hgroup>
         <FlipMove className={Styles.answersList}>
           <li>
             <img src="http://fordevs.herokuapp.com/static/img/logo-react.png" />
