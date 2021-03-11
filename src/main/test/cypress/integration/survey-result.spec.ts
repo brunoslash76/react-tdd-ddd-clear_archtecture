8d238c9eea52c3e7f335249d95a04bdcf7b09609
Bruno Russo <bruno.russo-c@adventhealth.com>
2021-03-11 12:17:23 -0300

    test: ensure SurveyResult present error if request fails

---
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -0,0 +1,20 @@
+import * as Http from '../utils/http-mocks'
+import * as Helper from '../utils/helpers'
+
+const path = /surveys/
+
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
+
+describe('Survey Result', () => {
+  beforeEach(() => {
+    cy.fixture('account').then(account => {
+      Helper.setLocalStorageItem('account',account)
+    })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    mockUnexpectedError()
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente')
+  })
+})
