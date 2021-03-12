a751efc129bbeed780d72a37f0b7d038533f1ad9
Bruno Russo <bruno.russo-c@adventhealth.com>
2021-03-12 10:02:38 -0300

    test: ensure RemoteSaveSurveyResult return AccessDeniedError on 403

--- src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
@@ -4,7 +4,7 @@ import { HttpClientSpy, mockRemoteSurveyResultModel } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { RemoteSaveSurveyResult } from './remote-save-survey-result'
 import { mockSaveSurveyResultParams } from '@/domain/test'
-// import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoteSaveSurveyResult
@@ -35,14 +35,14 @@ describe('RemoteSaveSurveyResult', () => {
     expect(httpClientSpy.body).toEqual(saveSurveyResultParams)
   })
 
-  // test('Should throw AccessDeniedError if HttpClient returns 403', async () => {
-  //   const { sut, httpClientSpy } = makeSut()
-  //   httpClientSpy.response = {
-  //     statusCode: HttpStatusCode.forbiden
-  //   }
-  //   const promise = sut.load()
-  //   await expect(promise).rejects.toThrow(new AccessDeniedError())
-  // })
+  test('Should throw AccessDeniedError if HttpClient returns 403', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.forbiden
+    }
+    const promise = sut.save(mockSaveSurveyResultParams())
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
+  })
 
   // test('Should throw UnexpectedError if HttpClient returns 404', async () => {
   //   const { sut, httpClientSpy } = makeSut()
